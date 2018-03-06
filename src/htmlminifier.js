fbfba2a1219e72c4e907e8bc2acf0c23a7e87f3a
Ryan Yurkanin <ryurkanin@getguru.com>
2018-03-06 12:11:54 -0500

    Another shot at fixing browserify

--- src/htmlminifier.js
+++ src/htmlminifier.js
@@ -2,9 +2,455 @@
 
 var CleanCSS = require('clean-css');
 var decode = require('he').decode;
-var HTMLParser = require('./htmlparser').HTMLParser;
+
+function createMap(values, ignoreCase) {
+  var map = {};
+  values.forEach(function(value) {
+    map[value] = 1;
+  });
+  return ignoreCase ? function(value) {
+    return map[value.toLowerCase()] === 1;
+  } : function(value) {
+    return map[value] === 1;
+  };
+}
+
+function createMapFromString(values, ignoreCase) {
+  return createMap(values.split(/,/), ignoreCase);
+}
+
+function makeMap(values) {
+  return createMapFromString(values, true);
+}
+
+// Regular Expressions for parsing tags and attributes
+var singleAttrIdentifier = /([^\s"'<>/=]+)/,
+    singleAttrAssigns = [/=/],
+    singleAttrValues = [
+      // attr value double quotes
+      /"([^"]*)"+/.source,
+      // attr value, single quotes
+      /'([^']*)'+/.source,
+      // attr value, no quotes
+      /([^ \t\n\f\r"'`=<>]+)/.source
+    ],
+    // https://www.w3.org/TR/1999/REC-xml-names-19990114/#NT-QName
+    qnameCapture = (function() {
+      var ncname = require('ncname').source.slice(1, -1);
+      return '((?:' + ncname + '\\:)?' + ncname + ')';
+    })(),
+    startTagOpen = new RegExp('^<' + qnameCapture),
+    startTagClose = /^\s*(\/?)>/,
+    endTag = new RegExp('^<\\/' + qnameCapture + '[^>]*>'),
+    doctype = /^<!DOCTYPE [^>]+>/i;
+
+var IS_REGEX_CAPTURING_BROKEN = false;
+'x'.replace(/x(.)?/g, function(m, g) {
+  IS_REGEX_CAPTURING_BROKEN = g === '';
+});
+
+// Empty Elements
+var empty = makeMap('area,base,basefont,br,col,embed,frame,hr,img,input,isindex,keygen,link,meta,param,source,track,wbr');
+
+// Inline Elements
+var inline = makeMap('a,abbr,acronym,applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,font,i,iframe,img,input,ins,kbd,label,map,noscript,object,q,s,samp,script,select,small,span,strike,strong,sub,sup,svg,textarea,tt,u,var');
+
+// Elements that you can, intentionally, leave open
+// (and which close themselves)
+var closeSelf = makeMap('colgroup,dd,dt,li,option,p,td,tfoot,th,thead,tr,source');
+
+// Attributes that have their values filled in disabled='disabled'
+var fillAttrs = makeMap('checked,compact,declare,defer,disabled,ismap,multiple,nohref,noresize,noshade,nowrap,readonly,selected');
+
+// Special Elements (can contain anything)
+var special = makeMap('script,style');
+
+// HTML5 tags https://html.spec.whatwg.org/multipage/indices.html#elements-3
+// Phrasing Content https://html.spec.whatwg.org/multipage/dom.html#phrasing-content
+var nonPhrasing = makeMap('address,article,aside,base,blockquote,body,caption,col,colgroup,dd,details,dialog,div,dl,dt,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,header,hgroup,hr,html,legend,li,menuitem,meta,ol,optgroup,option,param,rp,rt,source,style,summary,tbody,td,tfoot,th,thead,title,tr,track,ul');
+
+var reCache = {};
+
+function attrForHandler(handler) {
+  var pattern = singleAttrIdentifier.source +
+                '(?:\\s*(' + joinSingleAttrAssigns(handler) + ')' +
+                '[ \\t\\n\\f\\r]*(?:' + singleAttrValues.join('|') + '))?';
+  if (handler.customAttrSurround) {
+    var attrClauses = [];
+    for (var i = handler.customAttrSurround.length - 1; i >= 0; i--) {
+      attrClauses[i] = '(?:' +
+                       '(' + handler.customAttrSurround[i][0].source + ')\\s*' +
+                       pattern +
+                       '\\s*(' + handler.customAttrSurround[i][1].source + ')' +
+                       ')';
+    }
+    attrClauses.push('(?:' + pattern + ')');
+    pattern = '(?:' + attrClauses.join('|') + ')';
+  }
+  return new RegExp('^\\s*' + pattern);
+}
+
+function joinSingleAttrAssigns(handler) {
+  return singleAttrAssigns.concat(
+    handler.customAttrAssign || []
+  ).map(function(assign) {
+    return '(?:' + assign.source + ')';
+  }).join('|');
+}
+
+function HTMLParser(html, handler) {
+  var stack = [], lastTag;
+  var attribute = attrForHandler(handler);
+  var last, prevTag, nextTag;
+  while (html) {
+    last = html;
+    // Make sure we're not in a script or style element
+    if (!lastTag || !special(lastTag)) {
+      var textEnd = html.indexOf('<');
+      if (textEnd === 0) {
+        // Comment:
+        if (/^<!--/.test(html)) {
+          var commentEnd = html.indexOf('-->');
+
+          if (commentEnd >= 0) {
+            if (handler.comment) {
+              handler.comment(html.substring(4, commentEnd));
+            }
+            html = html.substring(commentEnd + 3);
+            prevTag = '';
+            continue;
+          }
+        }
+
+        // http://en.wikipedia.org/wiki/Conditional_comment#Downlevel-revealed_conditional_comment
+        if (/^<!\[/.test(html)) {
+          var conditionalEnd = html.indexOf(']>');
+
+          if (conditionalEnd >= 0) {
+            if (handler.comment) {
+              handler.comment(html.substring(2, conditionalEnd + 1), true /* non-standard */);
+            }
+            html = html.substring(conditionalEnd + 2);
+            prevTag = '';
+            continue;
+          }
+        }
+
+        // Doctype:
+        var doctypeMatch = html.match(doctype);
+        if (doctypeMatch) {
+          if (handler.doctype) {
+            handler.doctype(doctypeMatch[0]);
+          }
+          html = html.substring(doctypeMatch[0].length);
+          prevTag = '';
+          continue;
+        }
+
+        // End tag:
+        var endTagMatch = html.match(endTag);
+        if (endTagMatch) {
+          html = html.substring(endTagMatch[0].length);
+          endTagMatch[0].replace(endTag, parseEndTag);
+          prevTag = '/' + endTagMatch[1].toLowerCase();
+          continue;
+        }
+
+        // Start tag:
+        var startTagMatch = parseStartTag(html);
+        if (startTagMatch) {
+          html = startTagMatch.rest;
+          handleStartTag(startTagMatch);
+          prevTag = startTagMatch.tagName.toLowerCase();
+          continue;
+        }
+      }
+
+      var text;
+      if (textEnd >= 0) {
+        text = html.substring(0, textEnd);
+        html = html.substring(textEnd);
+      }
+      else {
+        text = html;
+        html = '';
+      }
+
+      // next tag
+      var nextTagMatch = parseStartTag(html);
+      if (nextTagMatch) {
+        nextTag = nextTagMatch.tagName;
+      }
+      else {
+        nextTagMatch = html.match(endTag);
+        if (nextTagMatch) {
+          nextTag = '/' + nextTagMatch[1];
+        }
+        else {
+          nextTag = '';
+        }
+      }
+
+      if (handler.chars) {
+        handler.chars(text, prevTag, nextTag);
+      }
+      prevTag = '';
+
+    }
+    else {
+      var stackedTag = lastTag.toLowerCase();
+      var reStackedTag = reCache[stackedTag] || (reCache[stackedTag] = new RegExp('([\\s\\S]*?)</' + stackedTag + '[^>]*>', 'i'));
+
+      html = html.replace(reStackedTag, function(all, text) {
+        if (stackedTag !== 'script' && stackedTag !== 'style' && stackedTag !== 'noscript') {
+          text = text
+            .replace(/<!--([\s\S]*?)-->/g, '$1')
+            .replace(/<!\[CDATA\[([\s\S]*?)]]>/g, '$1');
+        }
+
+        if (handler.chars) {
+          handler.chars(text);
+        }
+
+        return '';
+      });
+
+      parseEndTag('</' + stackedTag + '>', stackedTag);
+    }
+
+    if (html === last) {
+      throw new Error('Parse Error: ' + html);
+    }
+  }
+
+  if (!handler.partialMarkup) {
+    // Clean up any remaining tags
+    parseEndTag();
+  }
+
+  function parseStartTag(input) {
+    var start = input.match(startTagOpen);
+    if (start) {
+      var match = {
+        tagName: start[1],
+        attrs: []
+      };
+      input = input.slice(start[0].length);
+      var end, attr;
+      while (!(end = input.match(startTagClose)) && (attr = input.match(attribute))) {
+        input = input.slice(attr[0].length);
+        match.attrs.push(attr);
+      }
+      if (end) {
+        match.unarySlash = end[1];
+        match.rest = input.slice(end[0].length);
+        return match;
+      }
+    }
+  }
+
+  function handleStartTag(match) {
+    var tagName = match.tagName;
+    var unarySlash = match.unarySlash;
+
+    if (handler.html5 && lastTag === 'p' && nonPhrasing(tagName)) {
+      parseEndTag('', lastTag);
+    }
+
+    if (!handler.html5 && !inline(tagName)) {
+      while (lastTag && inline(lastTag)) {
+        parseEndTag('', lastTag);
+      }
+    }
+
+    if (closeSelf(tagName) && lastTag === tagName) {
+      parseEndTag('', tagName);
+    }
+
+    var unary = empty(tagName) || tagName === 'html' && lastTag === 'head' || !!unarySlash;
+
+    var attrs = match.attrs.map(function(args) {
+      var name, value, customOpen, customClose, customAssign, quote;
+      var ncp = 7; // number of captured parts, scalar
+
+      // hackish work around FF bug https://bugzilla.mozilla.org/show_bug.cgi?id=369778
+      if (IS_REGEX_CAPTURING_BROKEN && args[0].indexOf('""') === -1) {
+        if (args[3] === '') { delete args[3]; }
+        if (args[4] === '') { delete args[4]; }
+        if (args[5] === '') { delete args[5]; }
+      }
+
+      function populate(index) {
+        customAssign = args[index];
+        value = args[index + 1];
+        if (typeof value !== 'undefined') {
+          return '"';
+        }
+        value = args[index + 2];
+        if (typeof value !== 'undefined') {
+          return '\'';
+        }
+        value = args[index + 3];
+        if (typeof value === 'undefined' && fillAttrs(name)) {
+          value = name;
+        }
+        return '';
+      }
+
+      var j = 1;
+      if (handler.customAttrSurround) {
+        for (var i = 0, l = handler.customAttrSurround.length; i < l; i++, j += ncp) {
+          name = args[j + 1];
+          if (name) {
+            quote = populate(j + 2);
+            customOpen = args[j];
+            customClose = args[j + 6];
+            break;
+          }
+        }
+      }
+
+      if (!name && (name = args[j])) {
+        quote = populate(j + 1);
+      }
+
+      return {
+        name: name,
+        value: value,
+        customAssign: customAssign || '=',
+        customOpen: customOpen || '',
+        customClose: customClose || '',
+        quote: quote || ''
+      };
+    });
+
+    if (!unary) {
+      stack.push({ tag: tagName, attrs: attrs });
+      lastTag = tagName;
+      unarySlash = '';
+    }
+
+    if (handler.start) {
+      handler.start(tagName, attrs, unary, unarySlash);
+    }
+  }
+
+  function parseEndTag(tag, tagName) {
+    var pos;
+
+    // Find the closest opened tag of the same type
+    if (tagName) {
+      var needle = tagName.toLowerCase();
+      for (pos = stack.length - 1; pos >= 0; pos--) {
+        if (stack[pos].tag.toLowerCase() === needle) {
+          break;
+        }
+      }
+    }
+    // If no tag name is provided, clean shop
+    else {
+      pos = 0;
+    }
+
+    if (pos >= 0) {
+      // Close all the open elements, up the stack
+      for (var i = stack.length - 1; i >= pos; i--) {
+        if (handler.end) {
+          handler.end(stack[i].tag, stack[i].attrs, i > pos || !tag);
+        }
+      }
+
+      // Remove the open elements from the stack
+      stack.length = pos;
+      lastTag = pos && stack[pos - 1].tag;
+    }
+    else if (tagName.toLowerCase() === 'br') {
+      if (handler.start) {
+        handler.start(tagName, [], true, '');
+      }
+    }
+    else if (tagName.toLowerCase() === 'p') {
+      if (handler.start) {
+        handler.start(tagName, [], false, '', true);
+      }
+      if (handler.end) {
+        handler.end(tagName, []);
+      }
+    }
+  }
+}
+
 var RelateUrl = require('relateurl');
-var TokenChain = require('./tokenchain');
+
+
+function Sorter() {
+}
+
+Sorter.prototype.sort = function(tokens, fromIndex) {
+  fromIndex = fromIndex || 0;
+  for (var i = 0, len = this.keys.length; i < len; i++) {
+    var key = this.keys[i];
+    var token = key.slice(1);
+    var index = tokens.indexOf(token, fromIndex);
+    if (index !== -1) {
+      do {
+        if (index !== fromIndex) {
+          tokens.splice(index, 1);
+          tokens.splice(fromIndex, 0, token);
+        }
+        fromIndex++;
+      } while ((index = tokens.indexOf(token, fromIndex)) !== -1);
+      return this[key].sort(tokens, fromIndex);
+    }
+  }
+  return tokens;
+};
+
+function TokenChain() {
+}
+
+TokenChain.prototype = {
+  add: function(tokens) {
+    var self = this;
+    tokens.forEach(function(token) {
+      var key = '$' + token;
+      if (!self[key]) {
+        self[key] = [];
+        self[key].processed = 0;
+      }
+      self[key].push(tokens);
+    });
+  },
+  createSorter: function() {
+    var self = this;
+    var sorter = new Sorter();
+    sorter.keys = Object.keys(self).sort(function(j, k) {
+      var m = self[j].length;
+      var n = self[k].length;
+      return m < n ? 1 : m > n ? -1 : j < k ? -1 : j > k ? 1 : 0;
+    }).filter(function(key) {
+      if (self[key].processed < self[key].length) {
+        var token = key.slice(1);
+        var chain = new TokenChain();
+        self[key].forEach(function(tokens) {
+          var index;
+          while ((index = tokens.indexOf(token)) !== -1) {
+            tokens.splice(index, 1);
+          }
+          tokens.forEach(function(token) {
+            self['$' + token].processed++;
+          });
+          chain.add(tokens.slice(0));
+        });
+        sorter[key] = chain.createSorter();
+        return true;
+      }
+      return false;
+    });
+    return sorter;
+  }
+};
+
 var utils = require('./utils');
 
 function trimWhitespace(str) {
@@ -64,7 +510,6 @@ function collapseWhitespace(str, options, trimLeft, trimRight, collapseAll) {
   return lineBreakBefore + str + lineBreakAfter;
 }
 
-var createMapFromString = utils.createMapFromString;
 // non-empty tags that will maintain whitespace around them
 var inlineTags = createMapFromString('a,abbr,acronym,b,bdi,bdo,big,button,cite,code,del,dfn,em,font,i,ins,kbd,label,mark,math,nobr,object,q,rt,rp,s,samp,select,small,span,strike,strong,sub,sup,svg,textarea,time,tt,u,var');
 // non-empty tags that will maintain whitespace within them
